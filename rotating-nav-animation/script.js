d2932db0b5305348213c6b650f8007aaa703bf86
jason60810 <jason60810@gmail.com>
2022-12-05 23:42:41 +0800

    day-3

--- rotating-nav-animation/script.js
+++ rotating-nav-animation/script.js
@@ -1,7 +1,8 @@
-const open = document.getElementById('open')
-const close = document.getElementById('close')
-const container = document.querySelector('.container')
+const open = document.getElementById('open');
+const close = document.getElementById('close');
 
-open.addEventListener('click', () => container.classList.add('show-nav'))
+const container = document.querySelector('.container');
 
-close.addEventListener('click', () => container.classList.remove('show-nav'))
+open.addEventListener('click', () => container.classList.add('show-nav'));
+
+close.addEventListener('click', () => container.classList.remove('show-nav'));
